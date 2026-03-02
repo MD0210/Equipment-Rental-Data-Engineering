929723c7c7759ab2538d2e5a90fe860416bcff6c
Marc Daniel Ortiz <marcdanie.ortiz@gmail.com>
2026-03-02 18:18:54 +0800

    update logger and

--- equipment_rental/logger/logger.py
+++ equipment_rental/logger/logger.py
@@ -1,12 +1,38 @@
 import logging
+import os
+from datetime import datetime
+
 
 def get_logger(name="equipment_rental"):
     logger = logging.getLogger(name)
-    if not logger.hasHandlers():
-        logger.setLevel(logging.INFO)
-        ch = logging.StreamHandler()
-        ch.setLevel(logging.INFO)
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        ch.setFormatter(formatter)
-        logger.addHandler(ch)
+
+    if logger.hasHandlers():
+        return logger
+
+    logger.setLevel(logging.INFO)
+
+    # Create logs directory if not exists
+    log_dir = "logs"
+    os.makedirs(log_dir, exist_ok=True)
+
+    log_file = os.path.join(log_dir, "pipeline.log")
+
+    # Format
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
+
+    # Console handler
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.INFO)
+    console_handler.setFormatter(formatter)
+
+    # File handler
+    file_handler = logging.FileHandler(log_file)
+    file_handler.setLevel(logging.INFO)
+    file_handler.setFormatter(formatter)
+
+    logger.addHandler(console_handler)
+    logger.addHandler(file_handler)
+
     return logger
